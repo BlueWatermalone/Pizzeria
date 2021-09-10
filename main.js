61bcc4a1a58c9c02d23fe9c366a5826184d66670
BlueWatermalone <71526538+BlueWatermalone@users.noreply.github.com>
2021-09-10 19:33:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 menu_list_array = ["Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
 function getMenu() {
-    var htmldata;
-    htmldata = "<ol class='menulist'>"
+    var htmldata = "";
     menu_list_array.sort();
     for(var i=0;i<menu_list_array.length;i++){
-        htmldata = htmldata + '<li>' + menu_list_array[i] + '<li>'
+        htmldata = htmldata + menu_list_array[i] + '<br>'
     }
-    htmldata = htmldata + "</ol>"
     document.getElementById("display_menu").innerHTML = htmldata;
 }
 function add_item() {
